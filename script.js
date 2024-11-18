c6d2ea81e41f666132961c8dff12394fb3fc4137
Tracy Njoroge <tracywangari997@gmail.com>
2024-11-18 06:42:26 +0300

    Fetch and display city weather data using API

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+function updateWeather(response) {
+    let currentTemp = Math.round(response.data.temperature.current);
+    let temperatureEl = document.querySelector("#weather-temperature-value");
+    let city = document.querySelector("#weather-app-city");
+    let cityEl = response.data.city;
+    city.innerHTML = cityEl;
+    temperatureEl.innerHTML = currentTemp;
+}
+
+function searchCity(city) {
+    let apiKey = "db90o24154efb0eb442t9a7ef39fdafd";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit() {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let city = document.querySelector("#weather-app-city");
-    city.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Nairobi")
 
