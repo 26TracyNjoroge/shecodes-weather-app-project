60709612dad55a3d73fc599cb784bdcd9e19e735
Tracy Njoroge <tracywangari997@gmail.com>
2024-11-18 08:54:43 +0300

    Refactor weather app to fetch wind speed and humidity from API

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 function updateWeather(response) {
-    let currentTemp = Math.round(response.data.temperature.current);
+    let timestamp = response.data.time; 
     let temperatureEl = document.querySelector("#weather-temperature-value");
-    let city = document.querySelector("#weather-app-city");
-    let cityEl = response.data.city;
-    city.innerHTML = cityEl;
+    let cityEl = document.querySelector("#weather-app-city");
+    let descriptionEl = document.querySelector("#weather-description");
+    let humidityEl = document.querySelector("#humidity");
+    let windSpeedEl = document.querySelector("#wind-speed");
+    let dateEl = document.querySelector("#weather-date")
+
+    let date = new Date(timestamp * 1000);
+
+    let currentTemp = Math.round(response.data.temperature.current);
+    let city = response.data.city;
+    let description = response.data.condition.description;
+    let humidity = `${response.data.temperature.humidity}%`;
+    let windSpeed = `${response.data.wind.speed}km/h`;
+
+    dateEl.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
+    descriptionEl.innerHTML = description;
+    cityEl.innerHTML = city;
     temperatureEl.innerHTML = currentTemp;
+    humidityEl.innerHTML = humidity;
+    windSpeedEl.innerHTML = windSpeed;
+
+    console.log(date);
+    console.log(response.data.time)
 }
 
 function searchCity(city) {
